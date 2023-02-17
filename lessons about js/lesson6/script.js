5a768b9812759524b9763a4af7093703b9d1823f
stark2288915 <stark2288915@gamil.com>
2023-02-18 01:55:16 +0200

    Створив купу змінних поки не вимкнули світло.

--- lessons about js/lesson6/script.js
+++ lessons about js/lesson6/script.js
@@ -47,14 +47,172 @@ console.log(arrSplit);
 
 /*
 - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
-- створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
-    let nums = [11,21,3];
-sortNums(nums,'ascending') // [3,11,21]
-sortNums(nums,'descending') // [21,11,3]
 */
 
 let arrNums = [10,8,-7,55,987,-1011,0,1050,0];
 
 let numToStr = arrNums.map(element => {
-
+    return element.toString();
 });
+
+console.log(numToStr);
+
+/*
+- створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+    let nums = [11,21,3];
+
+
+
+
+sortNums(nums,'ascending') // [3,11,21]
+*/
+function  sortNumss(array, callback){
+    return callback(array);
+
+}
+console.log(sortNumss(arrNums, (array)=> {
+    return array.sort((a, b)=> a-b);
+}));
+
+
+
+
+
+
+
+/*sortNums(nums,'descending') // [21,11,3]*/
+
+console.log(sortNumss(arrNums, (array)=> {
+    return array.sort((a, b)=> b-a);
+}));
+
+/*================================================================================================================*/
+/*- є масив*/
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+
+/*-- відсортувати його за спаданням за monthDuration*/
+
+let fallMonthDuration = coursesAndDurationArray.sort((a, b)=>b['monthDuration']-a['monthDuration']);
+console.log(fallMonthDuration);
+
+
+
+
+
+
+/*-- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців*/
+
+let fifeMonth = coursesAndDurationArray.filter((element)=> element['monthDuration'] === 5);
+console.log(fifeMonth);
+
+
+
+
+/*-- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}*/
+
+let unshiftArr = coursesAndDurationArray.map((item, i)=>{
+    let newUser = {id: i+1,
+                   title: item.title,
+                   monthDuration: item.monthDuration
+    }
+    return newUser;
+});
+console.log(unshiftArr);
+
+
+/*
+===================================================================================================================
+*/
+
+
+/*описати колоду карт (від 6 до туза без джокерів)
+*/
+
+
+      /*cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+      value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+      color:'', // 'red','black'*/
+
+let value = [6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace', 'joker'];
+let color = ['red', 'black'];
+let cardSuit = ['spade', 'diamond', 'heart', 'clubs'];
+
+
+function deckOfCards (value, cardSuit, color) {
+    let deck = [];
+    for (let i = 0; i < value.length; i++) {
+        if(value[i] === 'joker'){
+            break;
+        }
+        for (const suitElem of cardSuit) {
+                if(suitElem === 'diamond' || suitElem === 'heart') {
+                    let card = {
+                        value: value[i],
+                        color: color[0],
+                        cardSuit: suitElem
+                    }
+                    deck.push(card);
+                }
+                    else {
+                    let card = {
+                        value: value[i],
+                        color: color[1],
+                        cardSuit: suitElem
+                    }
+                    deck.push(card);
+                    }
+                }
+            }
+    return deck;
+}
+
+let myCards = deckOfCards(value, cardSuit, color);
+console.log(deckOfCards(value, cardSuit, color));
+
+/*- знайти піковий туз*/
+
+let findAce = myCards.find(elem => elem['value'] === 'ace' || elem['value'] === 'spade');
+console.log(findAce);
+
+
+
+
+
+/*- всі шістки*/
+let findSix = myCards.filter(elem => elem['value'] === 6);
+console.log(findSix);
+
+
+
+/*- всі червоні карти*/
+let findRed = myCards.filter(elem => elem['color'] === 'red');
+console.log(findRed);
+
+
+
+
+/*- всі буби*/
+let findBuba = myCards.filter(elem => elem['cardSuit'] === 'diamond');
+console.log(findBuba);
+
+
+/*- всі трефи від 9 та більше*/
+
+let findKrest = myCards.filter(elem => elem['cardSuit'] === 'clubs' && elem['value'] > 8);
+console.log(findKrest);
+
+let findKrest1 = myCards.filter(elem => {
+    if(elem['cardSuit'] === 'clubs' &&){
+        return elem;
+    }
+});
+console.log(findKrest1);
+
