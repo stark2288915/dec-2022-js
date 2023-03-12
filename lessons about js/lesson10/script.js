528d2535aff1a0cbc1436856805479344f9dd4f2
stark2288915 <stark2288915@gamil.com>
2023-03-12 02:11:52 +0200

    Створив купу змінних поки не вимкнули світло.

---
+++ lessons about js/lesson10/script.js
@@ -0,0 +1,15 @@
+//Стоврити форму з трьома полями для name,sruname,age та кнопкою. При натисканні на кнопку зчитати данні з полів, та вивести об'єкт в документ. Іншими словами : заповниои форму, натиснули кнопку, під формою з'явився блок з вашим об'єктом
+
+
+/*
+let f1 = document.forms.f1;
+
+f1.onsubmit = function (eo){
+    eo.preventDefault();
+    document.write(f1.name.value);
+    document.write('<br>');
+    document.write(f1.username.value);
+    document.write('<br>');
+    document.write(f1.age.value);
+    document.write('<br>');
+}*/
